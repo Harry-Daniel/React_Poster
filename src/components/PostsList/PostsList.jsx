32674dc97b691b370bad14cd605b9d4d8aedd628
Harry-Daniel <hayiborkomla@gmail.com>
2024-05-13 13:54:35 +0000

    Adding Shared Header and More State Management

---
+++ src/components/PostsList/PostsList.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import NewPost from "../NewPost/NewPost";
+import Post from "../Post";
+import styles from "./PostsList.module.css";
+import Modal from "../Modal/Modal";
+
+function PostsList({isPosting, onStopPosting}) {
+ 
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function onAuthorChange(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <div>
+      {isPosting && <Modal onClose={onStopPosting}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={onAuthorChange}
+        />
+      </Modal> }
+      
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Senyo" body="Check out the full course!" />
+      </ul>
+    </div>
+  );
+}
+
+export default PostsList;
