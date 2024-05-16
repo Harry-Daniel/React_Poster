fe6041ac853fa48c441b91124268b87260e6aae4
Harry-Daniel <hayiborkomla@gmail.com>
2024-05-16 13:30:29 +0000

    Adding Loading State

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import MainHeader from '../components/MainHeader/MainHeader'
-import { Outlet } from 'react-router-dom'
+import { Outlet,useNavigation } from 'react-router-dom'
 
 function RootLayout() {
+  // const navigation =useNavigation();
+
+  
   return (
    <>
    <MainHeader/>
+   {/* <main> */}
+    {/* {navigation.state==='loading' && <p>Loading...</p>} */}
    <Outlet/>
+   {/* </main> */}
    </>
   )
 }
